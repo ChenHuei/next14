77b35e0aa775655c8d6e1416440898b62de9508d
LeoChen <leo_chen@mayohr.com>
2024-02-16 10:29:38 +0800

    rename route

--- src/app/leo/(group)/layout.tsx
+++ src/app/leo/(group)/layout.tsx
@@ -1,11 +1,11 @@
-export default function AboutLayout({
+export default function GroupLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
 		<div>
-			<div>About</div>
+			<div style={{ width: '100%', backgroundColor: '#da1884' }}>Group</div>
 			<div>{children}</div>
 		</div>
 	);
--- src/app/leo/layout.tsx
+++ src/app/leo/layout.tsx
@@ -5,7 +5,7 @@ export default function LeoLayout({
 }>) {
 	return (
 		<div>
-			<div>Leo</div>
+			<div style={{ width: '100%', backgroundColor: '#48a9c5' }}>Leo</div>
 			<div>{children}</div>
 		</div>
 	);
