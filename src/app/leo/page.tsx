533a0899904c675a27f078badd475376de396efa
LeoChen <leo_chen@mayohr.com>
2024-02-16 10:24:44 +0800

    add use router

--- src/app/leo/page.tsx
+++ src/app/leo/page.tsx
@@ -1,9 +1,13 @@
+'use client';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Leo() {
+	const router = useRouter();
 	return (
 		<div>
 			<Link href="/leo/profile#title">LINK TO PROFILE TITLE</Link>
+			<div onClick={() => router.push('/leo/me')}>REDIRECT TO Me</div>
 		</div>
 	);
 }
