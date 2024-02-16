60b76e348022da2b17619590eafb5cf99b236863
LeoChen <leo_chen@mayohr.com>
2024-02-16 10:46:56 +0800

    update color

--- src/app/leo/(group)/layout.tsx
+++ src/app/leo/(group)/layout.tsx
@@ -5,7 +5,7 @@ export default function GroupLayout({
 }>) {
 	return (
 		<div>
-			<div style={{ width: '100%', backgroundColor: '#da1884' }}>Group</div>
+			<div style={{ width: '100%', backgroundColor: '#49c0b6' }}>Group</div>
 			<div>{children}</div>
 		</div>
 	);
--- src/app/leo/layout.tsx
+++ src/app/leo/layout.tsx
@@ -5,7 +5,7 @@ export default function LeoLayout({
 }>) {
 	return (
 		<div>
-			<div style={{ width: '100%', backgroundColor: '#48a9c5' }}>Leo</div>
+			<div style={{ width: '100%', backgroundColor: '#0c3866' }}>Leo</div>
 			<div>{children}</div>
 		</div>
 	);
